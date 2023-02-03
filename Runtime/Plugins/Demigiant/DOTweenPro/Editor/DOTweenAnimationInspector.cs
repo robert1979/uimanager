ca2c9e3ca6578e995c7ce9dd846421693573072a
robert1979 <aoc1385@gmail.com>
2023-02-03 13:44:57 +0100

    update

--- Runtime/Plugins/Demigiant/DOTweenPro/Editor/DOTweenAnimationInspector.cs
+++ Runtime/Plugins/Demigiant/DOTweenPro/Editor/DOTweenAnimationInspector.cs
@@ -1,6 +1,5 @@
 // Author: Daniele Giardini - http://www.demigiant.com
 // Created: 2015/03/12 16:03
-
 using System;
 using System.Collections.Generic;
 using System.IO;
--- Runtime/Plugins/Demigiant/DOTweenPro/Editor/DOTweenPreviewManager.cs
+++ Runtime/Plugins/Demigiant/DOTweenPro/Editor/DOTweenPreviewManager.cs
@@ -1,6 +1,5 @@
 ﻿// Author: Daniele Giardini - http://www.demigiant.com
 // Created: 2015/03/12 16:03
-
 using System;
 using System.Collections.Generic;
 using DG.DemiEditor;
